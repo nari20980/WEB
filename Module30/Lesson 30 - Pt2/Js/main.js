94c95ac6632c486a1b7a4e242e7a1329a88b0fe5
nari20980 <narkrasniqi@gmail.com>
2026-05-25 19:18:23 +0200

    Module30

---
+++ Module30/Lesson 30 - Pt2/Js/main.js
@@ -0,0 +1,50 @@
+var wakeuptime;
+var dstime;
+var sleeptime;
+var noon=12;
+
+
+function showcurrentTime(){
+    var clock=document.getElementById('clock')
+    var currentTime=new Date();
+
+    
+    var hours=currentTime.getHours();
+    var minutes=currentTime.getMinutes();
+    var seconds=currentTime.getSeconds();
+
+    var meridian="AM"
+
+
+    if(hours>=12){
+        meridian="Pm"
+    }
+
+    var clocktime= hours + ":" + minutes+ ":" + seconds + ":" + meridian;
+    clock.innerText=clocktime;
+//NA DUHET ME THIRR FUNKSIONIN QE E NDRRON FOTON
+
+
+}
+function changeImage(){
+    var time=new Date().getHours();
+    console.log(time);
+
+
+
+    var image="img/ds_clock.png";
+    var imageHTML   =document.getElementById("timeImage")
+
+    if(time==wakeuptime){
+        image="img/ds_clock.png";
+        console.log("morning");
+    }else if(time==dstime){
+        image="img/class.gif";
+
+    }
+
+
+
+
+}
+
