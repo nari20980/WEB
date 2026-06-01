c6d2393685a379d1bc1d19010f6d732fde71118e
nari20980 <narkrasniqi@gmail.com>
2026-06-01 19:16:42 +0200

    Module31

---
+++ Module31/Js/main.js
@@ -0,0 +1,32 @@
+
+var a =8;
+do {
+    a= a + 1;
+    console.log(a);
+
+
+}while(a<15);
+
+console.log("................");
+var i=3;
+
+
+while (i<13){
+    i=i+1;
+    console.log(i);
+}
+
+var x=0;//1
+var y=0;//1,2
+
+while(y<4){
+    y++;//y=1,2,3,4
+    x+=y;//x=x+y 1,2+1=3,6,4+6=10
+    console.log(x);
+    console.log(y);
+
+}
+
+for(b=0;b<5;b++){
+    console.log("Vlera e b eshte :" + b)
+}
